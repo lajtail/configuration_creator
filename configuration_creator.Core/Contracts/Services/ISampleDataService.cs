52cab52dc4104225d2c19343c4d9364f702bd998
Lajtai Levente <levente.lajtai@kapsch.net>
2025-05-14 10:55:00 +0200

    New folder structure

---
+++ configuration_creator.Core/Contracts/Services/ISampleDataService.cs
@@ -0,0 +1,12 @@
+﻿using configuration_creator.Core.Models;
+
+namespace configuration_creator.Core.Contracts.Services;
+
+public interface ISampleDataService
+{
+    Task<IEnumerable<SampleOrder>> GetContentGridDataAsync();
+
+    Task<IEnumerable<SampleOrder>> GetListDetailsDataAsync();
+
+    Task<IEnumerable<SampleOrder>> GetGridDataAsync();
+}
